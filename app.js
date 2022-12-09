23b00701474430071a4121b38ecf4a972f7f49cf
andrewpolski <43200496+andrewpolski@users.noreply.github.com>
2022-12-09 21:26:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const expensesRouter = require("./routes/expenses");
+
+mongoose.connect(process.env.DB_URL, () => {
+  console.log("db connected");
+});
+const app = express();
+
+app.use(logger("dev"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/expenses", expensesRouter);
+
+module.exports = app;
